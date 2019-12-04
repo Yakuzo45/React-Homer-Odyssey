a515aa4261b1660bf63e32f283ec272fb8317b14
Yaku <tom.guibard@outlook.fr>
2019-12-04 12:04:36 +0100

    Quest 050 Homer's Odyssey

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -4,12 +4,12 @@ const connection = require('../../helpers/db');
 
 
 router.post('/signup', function(req, res, next) {
-    var user = [req.body.email, req.body.password, req.body.firstName, req.body.lastName];
+    var user = [req.body.email, req.body.password, req.body.firstname, req.body.lastname];
     connection.query('INSERT INTO users (email, password, name, lastname) VALUES (?, ?, ?, ?)', user , function (error, results, fields) {
         if(error) {
             res.status(500).json({ flash: error.message });
         } else {
-            res.status(200).json({ flash: "User has been signed up !" });
+            res.status(200).json({ flash: 'REQUEST OK' });
         }
 
     });
--- front/homer-odyssey-react/src/components/SignUp.js
+++ front/homer-odyssey-react/src/components/SignUp.js
@@ -43,7 +43,19 @@ class SignUp extends React.Component {
     };
 
     handleSubmit(){
-        console.log(JSON.stringify(this.state, 1, 1));
+        fetch("/auth/signup",
+            {
+                method:  'POST',
+                headers:  new Headers({
+                    'Content-Type':  'application/json'
+                }),
+                body:  JSON.stringify(this.state),
+            })
+            .then(res  =>  res.json())
+            .then(
+                res  =>  this.setState({"flash":  res.flash}),
+                err  =>  this.setState({"flash":  err.flash})
+            );
     };
 
 
@@ -52,16 +64,16 @@ class SignUp extends React.Component {
         return(
            <div className="form">
                <h1>{JSON.stringify(this.state, 1, 1)}</h1>
-               <form onSubmit={this.handleSubmit.bind(this)}>
+               <form method={"POST"} onSubmit={this.handleSubmit.bind(this)}>
                    <label htmlFor="email">Email :</label>
                    <input onChange={this.updateEmailField.bind(this)} type="email" name="email" /> <br/>
                    <label htmlFor="password">Password :</label>
                    <input onChange={this.updatePasswordField.bind(this)} type="password" name="password"/><br/>
                    <label htmlFor="passwordVerification">Password 2 :</label>
                    <input onChange={this.updatePasswordVerificationField.bind(this)} type="password" name="passwordVerification"/><br/>
-                   <label htmlFor="firstname">Prénom :</label>
+                   <label htmlFor="firstName">Prénom :</label>
                    <input onChange={this.updateFirstnameField.bind(this)} type="text" name="firstname"/><br/>
-                   <label htmlFor="lastname">Nom :</label>
+                   <label htmlFor="lastName">Nom :</label>
                    <input onChange={this.updateLastnameField.bind(this)} type="text" name="lastname"/><br/>
                    <input type="submit" value="Submit"/>
                </form>
